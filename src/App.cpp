e6e3c36599813994e9e77aa2371867a1976b5c51
Bach Nguyen <bach5000@gmail.com>
2018-11-11 14:30:20 -0600

    Add License

--- src/App.cpp
+++ src/App.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2018-2019 Bach Nguyen <bach5000@gmail.com>
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+
 #include <Alert.h>
 #include <Application.h>
 #include <FilePanel.h>
